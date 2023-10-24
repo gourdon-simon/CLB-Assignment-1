78fb11483479e4d1e78c72f9a4820400fe60271d
gourdon-simon <gourdon.simon.001@student.lu>
2023-10-24 13:57:03 +0200

    first commit

---
+++ Assignement1-CBA/README.txt
@@ -0,0 +1,21 @@
+Assignment 1: REST WS with Node.js
+
+First Part:
+Option 1: Download of the CSV file
+- CSV file found in directory of the script
+- access URL on localhost => http://localhost:8080/?date=1894
+- resolves:
+    [
+        {
+            "TIME_PERIOD": "1890-12-01",
+            "OBS_VALUE": "211481"
+        },
+        {
+            "TIME_PERIOD": "1895-12-02",
+            "OBS_VALUE": "217716"
+        }
+    ]
+
+    Status: 201
+
+(Tested with Postman)
--- Assignement1-CBA/app.js
+++ Assignement1-CBA/app.js
@@ -1,1 +1,89 @@
 // Assignment-1: Cloud-Based-Applications
+const { DateTime } = require("luxon");
+const http = require('http');
+var express = require('express');
+var app = express();
+const fs = require('fs');
+const parse = require('csv-parser');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+const filePath = "./LU1_DF_B1100_1.0_C01.A.csv"
+
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    const date = req.query.date;
+    let dateAsInteger = parseInt(date, 10)
+
+    if(dateAsInteger >= 1800 && dateAsInteger <= 2050){
+        iterateData(dateAsInteger)
+            .then((data) => {
+                if (data.length !== 0){
+                    res.status(201).send(data);
+                } else {
+                    res.status(404).send('No data found.')
+                }
+            }) 
+            .catch(() => {
+                res.status(500).send('Internal Server Error.')
+            });
+        
+    } else {
+        res.status(400).send('Invalid data.')
+    }
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at: http://${hostname}:${port}/`)
+});
+
+function iterateData(query) {
+    return new Promise((resolve, reject) => { 
+        const results = [];
+        const beforeDate = { date: null, diff: Infinity };
+        const afterDate = { date: null, diff: Infinity };
+        
+        fs.createReadStream(filePath)
+            .pipe(parse({ 
+                delimiter: ',',
+                headers: ['', '', '', 'TIME_PERIOD', 'OBS_VALUE'],
+                skipLines: 1
+            }))
+            .on('data', (row) => {
+                const { TIME_PERIOD, OBS_VALUE } = row;
+                const parsedDate = DateTime.fromFormat(TIME_PERIOD, 'yyyy-MM-dd');
+                let year = parsedDate.year;
+
+                if (year === query) {
+                    results.push({ TIME_PERIOD: parsedDate.toISODate(), OBS_VALUE });
+                } else if (year < query) {
+                    if (!beforeDate.date || parsedDate > beforeDate.date) {
+                        beforeDate.date = parsedDate;
+                        beforeDate.OBS_VALUE = OBS_VALUE;
+                    }
+                } else if (year > query) {
+                    if (!afterDate.date || parsedDate < afterDate.date) {
+                        afterDate.date = parsedDate;
+                        afterDate.OBS_VALUE = OBS_VALUE;
+                    }
+                }
+            })
+            .on('end', () => {
+                if (results.length === 0) {
+                    if (beforeDate.date) {
+                        results.push({ TIME_PERIOD: beforeDate.date.toISODate(), OBS_VALUE: beforeDate.OBS_VALUE });
+                    }
+                    if (afterDate.date) {
+                        results.push({ TIME_PERIOD: afterDate.date.toISODate(), OBS_VALUE: afterDate.OBS_VALUE });
+                    }
+                }
+                resolve(results);
+            })
+            .on('error', (err) => {
+                reject(err)
+            });
+    });
+}
+
